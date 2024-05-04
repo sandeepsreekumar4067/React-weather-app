872ddf1b1c13c56f2396165698fdfe7e31d49740
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-04 17:09:09 +0530

    added title components to project

--- src/components/interface.js
+++ src/components/interface.js
@@ -8,13 +8,19 @@ const Interface = () => {
                     Different kind of weather
                     <h2>24/7</h2>
                 </h1>
+                <div className="subtitles">
+                Step into a Weather Adventure: Discover Daily Forecasts, Live Updates, and Expert Insights for Every Climate Quest!
+                </div>
                 <div className="input-box">
-                    <FaSearch color='#31E1F7' size={35} />
-                    <input type="text" />
+                    <div className="spacer"></div>
+                    <FaSearch color='#31E1F7' size={35} opacity={1}/>
+                    <input type="text" placeholder='Enter the Location' />
                     <div className="search-button">
-                        
+                        Search
                     </div>
                 </div>
+                <br />
+                bestway to know your weather
             </div>
             <div className="weather-components"></div>
         </div>
