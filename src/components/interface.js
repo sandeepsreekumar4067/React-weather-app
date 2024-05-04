2ff3ffdc0346da91c5c0d033ded8cbe59a42d774
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-04 16:57:20 +0530

    body components partially complete

--- src/components/interface.js
+++ src/components/interface.js
@@ -1,8 +1,22 @@
 import '../Style/interface.css'
+import { FaSearch } from "react-icons/fa";
 const Interface = () => {
     return ( 
         <div className="interface">
-
+            <div className="title-components">
+                <h1>
+                    Different kind of weather
+                    <h2>24/7</h2>
+                </h1>
+                <div className="input-box">
+                    <FaSearch color='#31E1F7' size={35} />
+                    <input type="text" />
+                    <div className="search-button">
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="weather-components"></div>
         </div>
      );
 }
