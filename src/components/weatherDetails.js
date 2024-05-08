02ea1028c3ea6010deb66c4330d5fab4ac6a1798
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-08 23:56:55 +0530

    minor changes

--- src/components/weatherDetails.js
+++ src/components/weatherDetails.js
@@ -1,12 +1,48 @@
 import '../Style/weatherDetails.css'
+import earth from '../assets/earth.png'
 const WeatherDetails = () => {
     return (  
         <div className="weather-details">
             <div className="container1">
-                <div className="detail1"></div>
-                <div className="detail2"></div>
+                <div className="detail1">
+                    <div className="Weathertitle">
+                        Temperature
+                    </div>
+                    <div className="temperature">
+                        <div className="degree">37</div>
+                        <div className="degreetitle">degree celcius</div>
+                    </div>
+                </div>
+                <div className="detail2">
+                    <div className="detail2-container1">
+                        Local Weather Report
+                    </div>
+                    <div className="detail2-container2">
+                        <div className="image-tag">
+                            <img className='earth' src={earth} alt="" />
+                        </div>
+                        <div className="day-and-date">
+                            <div className="day">
+                                Sunday
+                            </div>
+                            <div className="weather-type">
+                                Sunny
+                            </div>
+                        </div>
+                    </div>
+                    <div className="detail2-container3">
+                        <div className="wind">
+                            Wind 24 nM West
+                        </div>
+                        <div className="humidity">
+                            24 ppm
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="container2"></div>
+            <div className="container2">
+                
+            </div>
         </div>
     );
 }
